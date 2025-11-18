f17d3ce72132a895656ebdc1b9fbcc089d9dba7f
aidanw <aidanconnorw@gmail.com>
2025-11-17 23:21:58 -0600

    added child card component and changed dynamicpage to take the card component and use it for a big and small card

--- unit1-final-project/src/components/DynamicPage.jsx
+++ unit1-final-project/src/components/DynamicPage.jsx
@@ -1,26 +1,42 @@
 import React, { useState } from "react";
 import Button from './Button';
+import Card from './Card';
 
 function DynamicPage({ title, data }) {
     const [selectedItem, setSelectedItem] = useState(null)
+
     return (
         <div className="dynamic-container">
             {selectedItem ? (
                 <>
-                <h1>{selectedItem.name}</h1>
-            <p>{selectedItem.description}</p>
-            <Button label="Back" onClick={() => setSelectedItem(null)} className="back-button" />
+
+                    <div className="selected-card">
+                        <Card
+                            title={selectedItem.name}
+                            description={selectedItem.description}
+                            image={selectedItem.image}
+                        />
+                    </div>
+
+                    <Button
+                        label="Back"
+                        onClick={() => setSelectedItem(null)}
+                        className="back-button"
+                    />
                 </>
             ) : (
                 <>
-                <h1>{title}</h1>
-                {data.map((item) => (
-                    <Button
-                    key={item.id}
-                    label={item.name}
-                    onClick={() => setSelectedItem(item)}
-                    />
-                ))}
+                    <h1>{title}</h1>
+
+                    {data.map((item) => (
+                        <Card
+                            key={item.id}
+                            title={item.name}
+                            shortdesc={item.shortdesc}
+                            image={item.image}
+                            onClick={() => setSelectedItem(item)}
+                        />
+                    ))}
                 </>
             )}
         </div>
