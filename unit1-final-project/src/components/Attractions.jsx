d5317b6aa1d31defef16565b8f2358b86106ba48
aidanw <aidanconnorw@gmail.com>
2025-11-20 14:34:18 -0600

    added handleAdd prop functon to both attractions and food components so that the places will get added to the bucketlist

--- unit1-final-project/src/components/Attractions.jsx
+++ unit1-final-project/src/components/Attractions.jsx
@@ -2,16 +2,16 @@ import Copyright from './Copyright';
 import DynamicPage from './DynamicPage';
 
 const attractionsData = [
-    { id: 1, name: "Gateway Arch", description: "Example description", shortdesc: "Short description", image: "/images/arch.jpg"},
-    { id: 2, name: "Sculpture Park", description: "Example description", shortdesc: "Example description", image: "/images/sculpture park.webp"},
-    { id: 3, name: "Botanical Gardens", description: "Example description", shortdesc: "Example description", image: "/images/botanicalgarden.jpg"},
-    { id: 4, name: "STL City Game", description: "Example description", shortdesc: "Example description", image: "/images/stlcitygame.jpg"},
+    { id: 1, category: "attraction", name: "Gateway Arch", description: "Example description", shortdesc: "Short description", image: "/images/arch.jpg"},
+    { id: 2, category: "attraction", name: "Sculpture Park", description: "Example description", shortdesc: "Short description", image: "/images/sculpture park.webp"},
+    { id: 3, category: "attraction", name: "Botanical Gardens", description: "Example description", shortdesc: "Short description", image: "/images/botanicalgarden.jpg"},
+    { id: 4, category: "attraction", name: "STL City Game", description: "Example description", shortdesc: "Short description", image: "/images/stlcitygame.jpg"},
 ];
 
-function Attractions() {
+function Attractions( { handleAdd }) {
     return(
         <div>
-            <DynamicPage title="Attractions" data={attractionsData} />
+            <DynamicPage title="Attractions" data={attractionsData} onAdd={handleAdd}/>
             <Copyright />
         </div>
     );
