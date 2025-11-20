27371beb7f4aaec2b031f4055ed8c757c4444982
aidanw <aidanconnorw@gmail.com>
2025-11-20 14:30:15 -0600

    added an add-button to the dynamicPage component

--- unit1-final-project/src/components/DynamicPage.jsx
+++ unit1-final-project/src/components/DynamicPage.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import Button from './Button';
 import Card from './Card';
 
-function DynamicPage({ title, data }) {
+function DynamicPage({ title, data, onAdd }) {
     const [selectedItem, setSelectedItem] = useState(null)
+
 
     return (
         <div className="dynamic-container">
@@ -23,21 +24,32 @@ function DynamicPage({ title, data }) {
                         onClick={() => setSelectedItem(null)}
                         className="back-button"
                     />
+
+                    <Button
+                        label="Add to Bucket List"
+                        onClick={() => {
+                            onAdd(selectedItem);       // add it
+                            setSelectedItem(null);     // go back
+                        }}
+                        className="add-button"
+                    />
+
                 </>
             ) : (
                 <>
                     <h1>{title}</h1>
-                        
+
                     <div className="card-grid">
-                    {data.map((item) => (
-                        <Card
-                            key={item.id}
-                            title={item.name}
-                            shortdesc={item.shortdesc}
-                            image={item.image}
-                            onClick={() => setSelectedItem(item)}
-                        />
-                    ))}
+                        {data.map((item) => (
+                            <Card
+                                key={item.id}
+                                title={item.name}
+                                shortdesc={item.shortdesc}
+                                image={item.image}
+                                onClick={() => setSelectedItem(item)}
+
+                            />
+                        ))}
                     </div>
                 </>
             )}
