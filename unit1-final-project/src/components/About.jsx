05033a0a0eea3c4e5bb6e95bae1827b95aa0c77f
aidanw <aidanconnorw@gmail.com>
2025-11-20 15:10:17 -0600

    added a contact us table on the about page, also added social media icons file

--- unit1-final-project/src/components/About.jsx
+++ unit1-final-project/src/components/About.jsx
@@ -1,4 +1,3 @@
-import stlpic from '../assets/stlpic.jpg'
 import Copyright from './Copyright';
 
 function About() {
@@ -6,14 +5,38 @@ function About() {
     <div className="aboutus">
         <h1>About Us</h1>
         <img
-            src={stlpic} alt="St. Louis skyline at night" className="skyline"/>   
+            src="/images/stlpic.jpg" alt="St. Louis skyline at night" className="skyline"/>   
         <p>At Meet Me in STL, our job is to provide the best 
           experience for you when traveling to St.Louis.   
         </p>
         <footer>
             <h3>Contact Us!</h3>
-            <p>Email: info@meetmeinstl.com</p>
-            <p>Phone: (314) 222-2222</p>
+            <table className="contact-table">
+                <tbody>
+                    <tr>
+                        <td>Email:</td>
+                        <td>info@meetmeinstl.com</td>
+                    </tr>
+                    <tr>
+                        <td>Phone:</td>
+                        <td>(314)222-2222</td>
+                    </tr>
+                    <tr>
+                        <td>Facebook:</td>
+                        <td>
+                            <img src="/images/react-icons/square-facebook-brands-solid-full.svg" alt="Facebook" className="social-icon" />
+                            MeetMeInSTL
+                            </td>
+                    </tr>
+                    <tr>
+                        <td>Instagram:</td>
+                         <td>
+                            <img src="/images/react-icons/square-instagram-brands-solid-full.svg" alt="Instagram" className="social-icon" />
+                            @MeetMeInSTL
+                            </td>
+                    </tr>
+                </tbody>
+            </table>
             <Copyright />
         </footer>
         
@@ -21,4 +44,4 @@ function About() {
 );
 }
 
-export default About;
+export default About;    
