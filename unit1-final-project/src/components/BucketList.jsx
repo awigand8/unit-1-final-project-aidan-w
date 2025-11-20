d077242c3e4d49378b2975e2e363c4402878d54f
aidanw <aidanconnorw@gmail.com>
2025-11-19 20:07:04 -0600

    created BucketList component and added routing, also deleted unneeded files

---
+++ unit1-final-project/src/components/BucketList.jsx
@@ -0,0 +1,12 @@
+import Copyright from "./Copyright";
+
+function BucketList() {
+    return (
+        <div>
+        <h1>Bucket List</h1>
+        <Copyright />
+        </div>
+    );
+}
+
+export default BucketList;
