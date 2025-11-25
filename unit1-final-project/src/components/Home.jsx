965dc79423aa9482b5c65c9fe1b9679ed1cb2836
aidanw <aidanconnorw@gmail.com>
2025-11-24 20:19:08 -0600

    added introduction for home page

--- unit1-final-project/src/components/Home.jsx
+++ unit1-final-project/src/components/Home.jsx
@@ -1,10 +1,16 @@
 import Copyright from './Copyright';
 
 function Home() {
-    return(
-        <div>
+    return (
+        <div className="home-intro">
             <h1>Home</h1>
-            <p></p>
+            <p>
+                Explore the best of St. Louis with us! From iconic attractions like the Gateway Arch to hidden gems around the city, we're here to help you plan an unforgettable trip.
+                Discover delicious food spots, exciting events, and experiences that make STL truly unique.
+            </p>
+            <p>
+                Whether you're a first-time visitor or a longtime local, Meet Me in STL is your ultimate guide to everything this vibrant city has to offer. Let's make your next adventure in St. Louis one to remember!
+            </p>
             <Copyright />
         </div>
     );
